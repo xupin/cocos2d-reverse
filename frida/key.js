9d83aa0d69fc88e07a157e6e48cbcb15bb741606
Mark Xu <pin.xu@centurygame.com>
2024-05-13 21:53:46 +0800

    opt

--- frida/key.js
+++ frida/key.js
@@ -1,26 +1,26 @@
 /// <reference path='./index.d.ts'/>
 
 Java.perform(function () {
-  function intercept() {
-    const libName = "libcocos2dlua.so";
-    const funcName = "_Z13xxtea_decryptPhjS_jPj";
-    const ptr = Module.findExportByName(libName, funcName);
-    if (!ptr) {
-      setTimeout(intercept, 500);
-      return;
-    }
-    Interceptor.attach(ptr, {
-      onEnter: function (args) {
-        console.log("----------------BEGIN----------------");
-        console.log("Key:");
-        console.log(args[2].readCString(16));
-      },
-      onLeave: function (retval) {
-        console.log("Encrypted:");
-        console.log(retval.readByteArray(16));
-        console.log("-----------------END-----------------");
-      },
-    });
-  }
-  intercept();
+	function intercept() {
+		const libName = "libcocos2dlua.so";
+		const funcName = "_Z13xxtea_decryptPhjS_jPj";
+		const ptr = Module.findExportByName(libName, funcName);
+		if (!ptr) {
+			setTimeout(intercept, 500);
+			return;
+		}
+		Interceptor.attach(ptr, {
+			onEnter: function (args) {
+				console.log("----------------BEGIN----------------");
+				console.log("Key:");
+				console.log(Memory.readUtf8String(args[2]));
+			},
+			onLeave: function (retval) {
+				console.log("Encrypted:");
+				console.log(retval.readByteArray(16));
+				console.log("-----------------END-----------------");
+			},
+		});
+	}
+	intercept();
 });
